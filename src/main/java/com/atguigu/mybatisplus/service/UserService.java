94ad1dfcaf8cbb627139a19e4139c7e3737b0439
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-02 15:26:32 +0800

    DataSource service层 垂直分表(某些字段不经常访问 所以使用垂直分割)

---
+++ src/main/java/com/atguigu/mybatisplus/service/UserService.java
@@ -0,0 +1,11 @@
+package com.atguigu.mybatisplus.service;
+
+import com.atguigu.mybatisplus.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService extends IService<User> {
+
+
+}
