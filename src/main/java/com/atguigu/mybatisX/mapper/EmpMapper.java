c1d0f6e740f8658d4c2a9a46db6daa2742a5f968
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-02 21:35:12 +0800

    DataSource 测试其他的方法 试几次就会出来了

--- src/main/java/com/atguigu/mybatisX/mapper/EmpMapper.java
+++ src/main/java/com/atguigu/mybatisX/mapper/EmpMapper.java
@@ -1,4 +1,5 @@
 package com.atguigu.mybatisX.mapper;
+import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.atguigu.mybatisX.pojo.Emp;
@@ -42,6 +43,41 @@ public interface EmpMapper extends BaseMapper<Emp> {
     int deleteByEidAndDelFlagEqualsAndEmpNameLike(@Param("eid") Integer eid,
                                                   @Param("delFlag") String delFlag,
                                                   @Param("empName") String empName);
+
+    /**
+     * 开斋节=和年龄之间选择
+     *
+     * @param eid      开斋节
+     * @param beginAge 开始年龄
+     * @param endAge   时代结束
+     * @return {@link List}<{@link Emp}>
+     */
+    List<Emp> selectByEidEqualsAndAgeBetween(@Param("eid") Integer eid,
+                                             @Param("beginAge") Integer beginAge,
+                                             @Param("endAge") Integer endAge);
+
+    /**
+     * 更新年龄和性别
+     *
+     * @param age 年龄
+     * @param sex 性
+     * @return int
+     */
+    int updateAgeAndSex(@Param("age") Integer age, @Param("sex") String sex);
+
+    /**
+     * 更新年龄和emp名字开斋节和德尔国旗
+     *
+     * @param age     年龄
+     * @param empName emp名字
+     * @param eid     开斋节
+     * @param delFlag 德尔国旗
+     * @return int
+     */
+    int updateAgeAndEmpNameByEidAndDelFlag(@Param("age") Integer age,
+                                           @Param("empName") String empName,
+                                           @Param("eid") Integer eid,
+                                           @Param("delFlag") String delFlag);
 }
 
 
