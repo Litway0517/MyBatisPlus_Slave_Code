bf1a92b731d2cc5cb103691c3fb31f79aa24becb
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-02 14:32:55 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/mybatisplus/MyBatisPlusSlaveCodeApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusSlaveCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusSlaveCodeApplication.class, args);
+    }
+
+}
