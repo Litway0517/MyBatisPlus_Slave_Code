4d70762e8933565bdc9187a5a80972f085608760
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-02 21:28:14 +0800

    DataSource 测试MybatisX Plugin的mapper层方法生成 注意写完一个字段就应该写这个字段的判断条件? 比如Equals|GreaterThan|LessThan|Like...

--- src/main/java/com/atguigu/mybatisX/mapper/EmpMapper.java
+++ src/main/java/com/atguigu/mybatisX/mapper/EmpMapper.java
@@ -1,4 +1,5 @@
 package com.atguigu.mybatisX.mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.atguigu.mybatisX.pojo.Emp;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -10,7 +11,37 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity com.atguigu.mybatisX.pojo.Emp
 */
 public interface EmpMapper extends BaseMapper<Emp> {
+    /**
+     * 插入选择性
+     *
+     * @param emp 电磁脉冲
+     * @return int
+     */
+    int insertSelective(Emp emp);
 
+    /**
+     * 删除开斋节和德尔国旗大于和emp名字
+     *
+     * @param eid     开斋节
+     * @param delFlag 德尔国旗
+     * @param empName emp名字
+     * @return int
+     */
+    int deleteByEidAndDelFlagGreaterThanAndEmpName(@Param("eid") Integer eid,
+                                                   @Param("delFlag") String delFlag,
+                                                   @Param("empName") String empName);
+
+    /**
+     * 删除开斋节和德尔国旗=和emp名字
+     *
+     * @param eid     开斋节
+     * @param delFlag 德尔国旗
+     * @param empName emp名字
+     * @return int
+     */
+    int deleteByEidAndDelFlagEqualsAndEmpNameLike(@Param("eid") Integer eid,
+                                                  @Param("delFlag") String delFlag,
+                                                  @Param("empName") String empName);
 }
 
 
