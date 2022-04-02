2e17016b9ae399b9cbbe04220f9dafbb7068ad30
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-02 16:31:15 +0800

    DataSource Service注解标注在Impl类上面 不然会报错

--- src/main/java/com/atguigu/mybatisplus/service/UserService.java
+++ src/main/java/com/atguigu/mybatisplus/service/UserService.java
@@ -5,7 +5,6 @@ import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.springframework.stereotype.Service;
 
-@Service
 // DS注解用于标识此Service使用的数据源是哪一个
 @DS("master")
 public interface UserService extends IService<User> {
--- src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
+++ src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
@@ -4,7 +4,10 @@ import com.atguigu.mybatisplus.entity.User;
 import com.atguigu.mybatisplus.mapper.UserMapper;
 import com.atguigu.mybatisplus.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
 
