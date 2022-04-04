14907a929606ceb65d1f2ef751b50f38eb65de52
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-04 22:14:28 +0800

    测试 有问题待查

---
+++ src/test/java/com/atguigu/mybatiscodehelperpro/mapper/ProductMapperTest.java
@@ -0,0 +1,28 @@
+package com.atguigu.mybatiscodehelperpro.mapper;
+
+import com.atguigu.mybatiscodehelperpro.pojo.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductMapperTest {
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Test
+    public void testProductMapper() {
+        Product product = new Product();
+        product.setName("G15 I-12700H");
+        product.setPrice(8299);
+        int insert = productMapper.insert(product);
+        System.out.println("影响行数 -> " + insert);
+        System.out.println("返回的主键userGeneratorKeys -> " + product);
+    }
+
+
+
+}
