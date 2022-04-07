aa558e852db18704f3260f525dc5e3f2283c65c8
Mingw0717-Dell <xmw0517@gmail.com>
2022-04-07 21:52:44 +0800

    点亮

--- src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
+++ src/main/java/com/atguigu/mybatisplus/mapper/UserMapper.java
@@ -4,6 +4,12 @@ import com.atguigu.mybatisplus.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * 用户映射器
+ *
+ * @author DELL_
+ * @date 2022/04/07
+ */
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
